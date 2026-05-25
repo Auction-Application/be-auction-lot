03b8d79e97ee4eee7b1b50e10c78130e62711027
shaishav-rgb <shaishavpaudel@gmail.com>
2026-05-25 07:34:44 +0545

    refactor: refactor setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env found, when using environment variables")
+		fmt.Println(err)
+		return
+	}
 
-	fmt.Println("This is auction item running")
+	fmt.Println("gRPC server up and ready...")
+	// err := Run(9001, server.NewUserServer())
+	// if err != nil {
+	// 	fmt.Println("Cannot run the server")
+	// 	return
+	// }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Auction-Application/be-auction-item
 
 go 1.26.3
+
+require github.com/joho/godotenv v1.5.1
