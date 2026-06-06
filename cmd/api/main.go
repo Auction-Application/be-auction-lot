cbda865800601bb9c019dfd9addf922604879a34
shaishav-rgb <shaishavpaudel@gmail.com>
2026-06-06 18:34:15 +0545

    feat: add s3 upload

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 
@@ -16,9 +17,25 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	s3Storage, err := server.NewS3Storage()
+	if err != nil {
+		fmt.Println("Error in creating s3 client")
+	}
+	// err = s3Storage.UploadFile(context.TODO(), "arn:aws:s3:ap-south-1:433154991296:accesspoint/auction-lot-service-access-point", "test1", "files/aws_logo.png")
+	// if err != nil {
+	// 	fmt.Println("Error in uploading file")
+	// }
+
+	presignedHttpRequest, err := s3Storage.CreatePresignedPutObjectUrl(context.Background(), "arn:aws:s3:ap-south-1:433154991296:accesspoint/auction-lot-service-access-point", "test2", 3600)
+
+	if err != nil {
+		fmt.Println("Error in creating preSigned put s3 url")
+	}
+
+	fmt.Printf("%#v", presignedHttpRequest)
 
 	fmt.Println("grpc server about to be run")
-	err := Run(9002, server.NewLotServer())
+	err = Run(9002, server.NewLotServer())
 	if err != nil {
 		fmt.Println("Cannot run the grpc server")
 		fmt.Println(err)
---
+++ internal/server/imageUpload.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
+)
+
+type S3Storage struct {
+	s3Client  *s3.Client
+	Presigner *s3.PresignClient
+}
+
+func NewS3Storage() (*S3Storage, error) {
+	ctx := context.Background()
+	sdkConfig, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("load aws config: %w", err)
+	}
+	s3Client := s3.NewFromConfig(sdkConfig)
+	return &S3Storage{
+		s3Client:  s3Client,
+		Presigner: s3.NewPresignClient(s3Client),
+	}, nil
+}
+
+func (s3Storage S3Storage) UploadFile(ctx context.Context, bucketName string, objectKey string, fileName string) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", fileName, err)
+	} else {
+		_, err := s3Storage.s3Client.PutObject(ctx, &s3.PutObjectInput{
+			Bucket:      aws.String(bucketName),
+			Key:         aws.String(objectKey),
+			Body:        file,
+			ContentType: aws.String("image/png"),
+		})
+
+		if err != nil {
+			var apiErr smithy.APIError
+			if errors.As(err, &apiErr) && apiErr.ErrorCode() == "EntityTooLarge" {
+				log.Printf("Error while uploading object to %s. The object is too large.\n"+
+					"To upload objects larger than 5GB, use the S3 console (160GB max)\n"+
+					"or the multipart upload API (5TB max).", bucketName)
+			} else {
+				log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
+					fileName, bucketName, objectKey, err)
+			}
+		}
+
+	}
+	defer file.Close()
+	return nil
+}
+
+func (s3Storage S3Storage) CreatePresignedPutObjectUrl(
+	ctx context.Context, bucketName string, objectKey string, lifetimeSecs int64) (*v4.PresignedHTTPRequest, error) {
+	request, err := s3Storage.Presigner.PresignPutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to put %v:%v. Here's why: %v\n",
+			bucketName, objectKey, err)
+	}
+	return request, err
+}
